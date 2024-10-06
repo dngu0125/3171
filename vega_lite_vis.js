c8f9a61bea79e12ebe4e3a7f88f30a5ca93c2343
dngu0125 <dngu0125@student.monash.edu>
2024-10-07 03:25:55 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,8 @@
-var vg_1 = "SuicideRate.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
+var vg_1 = "SuicideRate.json";  
+vegaEmbed("#vis1", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_2 = "BubbleChart.json";  
+vegaEmbed("#vis2", vg_2).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
