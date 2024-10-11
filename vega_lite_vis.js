8440faa612c6845534b10792e373d0c362728315
dngu0125 <dngu0125@student.monash.edu>
2024-10-11 13:48:38 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -6,3 +6,19 @@ var vg_2 = "BubbleChart.json";
 vegaEmbed("#vis2", vg_2).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+var vg_3 = "Ranking.json";  
+vegaEmbed("#vis3", vg_3).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_4 = "RankingLowest.json";  
+vegaEmbed("#vis4", vg_4).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_5 = "test.json";  
+vegaEmbed("#vis5", vg_5).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_6 = "Bubble2.json";  
+vegaEmbed("#vis6", vg_6).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
