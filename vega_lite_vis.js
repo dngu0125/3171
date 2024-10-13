d0672e501774b9285db8b60ba27b966b2a4b0083
dngu0125 <dngu0125@student.monash.edu>
2024-10-13 22:44:26 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "SuicideRate.json";  
+var vg_1 = "Bubble2.json";  
 vegaEmbed("#vis1", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
@@ -6,19 +6,31 @@ var vg_2 = "BubbleChart.json";
 vegaEmbed("#vis2", vg_2).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-var vg_3 = "Ranking.json";  
+var vg_3 = "GDP.json";  
 vegaEmbed("#vis3", vg_3).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-var vg_4 = "RankingLowest.json";  
+var vg_4 = "gender.json";  
 vegaEmbed("#vis4", vg_4).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-var vg_5 = "test.json";  
+var vg_5 = "Ranking.json";  
 vegaEmbed("#vis5", vg_5).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-var vg_6 = "Bubble2.json";  
+var vg_6 = "RankingLowest.json";  
 vegaEmbed("#vis6", vg_6).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_7 = "Suiciderate.json";  
+vegaEmbed("#vis7", vg_7).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_8 = "test.json";  
+vegaEmbed("#vis8", vg_8).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_9 = "bubble3.json";  
+vegaEmbed("#vis9", vg_9).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
